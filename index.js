3ea28608b393251776c85e061eecc174ecbe766f
AngelGPaulin <apaulin14@alumnos.uaq.mx>
2024-04-25 22:39:57 -0600

    Mensaje Port

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ app.get('/', (req, res) => {
     res.send(`Hello World from Port ${port}`);
 });
 
-app.listen(3000, () =>{
-    console.info(`Database is running on port ${port}`);
+app.get('/myname', (req, res) => {
+    const name = 'Angel';
+    res.send(`Hello World! My name is ${name}`);
 });
+
+app.listen(port, () => {
+    console.info(`Server is running on port ${port}`);
+});
+
